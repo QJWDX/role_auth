0b4f7476dd179191b8f98766e6e507f7df50f744
lizhixiong <lizhixiong@runone.com>
2021-01-04 10:45:42 +0800

    11

--- database/migrations/2019_10_12_114754_menus.php
+++ database/migrations/2019_10_12_114754_menus.php
@@ -20,10 +20,10 @@ class Menus extends Migration
             $table->string("icon")->nullable()->comment("图标");
             $table->string("path")->comment("url");
             $table->string("component")->nullable()->comment('前端组建地址');
-            $table->tinyInteger("is_show")->default(1)->comment("是否展示（默认是）(1-是，2-否)");
+            $table->tinyInteger("status")->default(1)->comment("是否展示（默认是）(1-是，2-否)");
             $table->tinyInteger("is_related_route")->default(2)->comment("是否关联路由");
             $table->tinyInteger("is_default")->default(2)->comment("是否为默认路由，即系统的首页(1-是，2-否)");
-            $table->integer("sort_field")->default(0)->comment("排序，0为顶级");
+            $table->integer("sort")->default(0)->comment("排序，0为顶级");
             $table->timestamps();
             $table->comment = '菜单表';
         });
